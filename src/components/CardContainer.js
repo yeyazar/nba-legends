4f0afdf340d585ea8ae12abe6a41b62ebf4080f1
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-19 19:14:14 +0100

    push3

---
+++ src/components/CardContainer.js
@@ -0,0 +1,12 @@
+import PlayerCard from "./PlayerCard"
+
+const CardContainer = () => {
+    return (
+        <div>
+            <h1>Container</h1>
+            <PlayerCard/>
+        </div>
+    )
+}
+
+export default CardContainer
---
+++ src/components/PlayerCard.js
@@ -0,0 +1,9 @@
+const PlayerCard = () => {
+    return (
+        <div>
+            <h1>Card</h1>
+        </div>
+    )
+}
+
+export default PlayerCard
